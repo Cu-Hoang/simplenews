447359395f1436da2397bfc8c584ef0a288840ad
Cu-Hoang <cungochoang2003@gmail.com>
2025-04-16 16:35:00 +0700

    chore: initialize auth-service

---
+++ apps/auth-service/src/app/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from '../database/database.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    DatabaseModule,
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+  exports: [AuthService],
+})
+export class AuthModule {}
---
+++ apps/auth-service/src/app/refresh-token/refresh-token.entity.ts
@@ -0,0 +1,20 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { v7 as uuidv7 } from 'uuid';
+
+@Entity()
+export class RefreshToken {
+  @PrimaryColumn()
+  id: string = uuidv7();
+
+  @Column('uuid')
+  user_id: string;
+
+  @Column('uuid')
+  jti: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Column({ type: 'timestamp', nullable: false })
+  expiresAt: Date;
+}
